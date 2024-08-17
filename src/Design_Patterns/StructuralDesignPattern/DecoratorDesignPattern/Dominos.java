64952e9b48216101e28001096cb8b54a663b4e82
h493 <chhikarahimanshu21@gmail.com>
2024-08-17 15:42:31 +0530

    decorator design pattern

--- src/Design_Patterns/StructuralDesignPattern/DecoratorDesignPattern/Dominos.java
+++ src/Design_Patterns/StructuralDesignPattern/DecoratorDesignPattern/Dominos.java
@@ -3,6 +3,7 @@ package Design_Patterns.StructuralDesignPattern.DecoratorDesignPattern;
 import Design_Patterns.StructuralDesignPattern.DecoratorDesignPattern.BasePizza.BasePizza;
 import Design_Patterns.StructuralDesignPattern.DecoratorDesignPattern.BasePizza.Margarita;
 import Design_Patterns.StructuralDesignPattern.DecoratorDesignPattern.Topping.ExtraChesse;
+import Design_Patterns.StructuralDesignPattern.DecoratorDesignPattern.Topping.Mushroom;
 
 public class Dominos {
 
@@ -10,7 +11,9 @@ public class Dominos {
         BasePizza margariate = new Margarita();
 
         BasePizza extraCheeseMargarita = new ExtraChesse(margariate);
+        BasePizza mushromExtraCheeseMargarita = new Mushroom(extraCheeseMargarita);
 
         System.out.println(extraCheeseMargarita.cost());
+        System.out.println(mushromExtraCheeseMargarita.cost());
     }
 }
